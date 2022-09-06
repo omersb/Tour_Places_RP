4f97fd97f0d82020953ea5ecc20693cfbe503a79
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-09-06 18:17:23 +0300

    main scss

--- solution/src/components/Main/Main.jsx
+++ solution/src/components/Main/Main.jsx
@@ -4,11 +4,10 @@ import { data } from "../../helpers/data";
 import Card from "./Card";
 
 const Main = () => {
-  console.log(data);
   return (
     <div className="card-container">
-      {data.map((item, index) => (
-        <Card {...item} key={index} />
+      {data.map((item) => (        
+        <Card {...item} key={item.id} />
       ))}
     </div>
   );
